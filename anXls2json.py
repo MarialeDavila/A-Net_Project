01188f5df122805b072a3ae2f39c4571eec5dc3d
Maria Alejandra Davila Salazar <mads519@gmail.com>
2015-02-07 17:24:47 -0500

    Changes on way to create major nodes, to avoid error

--- anXls2json.py
+++ anXls2json.py
@@ -9,14 +9,11 @@ from xls file to json
 def add_node_to_structure(results_tmp, bias, all_new_nodes,
                           all_parent_categories, parents):
     counter = len(results_tmp)
-    list_new_nodes = []
     for idx in range(0, len(all_new_nodes)):
         new_node = all_new_nodes[idx]
         idx_new_node = all_new_nodes.index(new_node)
         if idx_new_node < idx:
             continue
-        else:
-            list_new_nodes.append(new_node)
 
         parent_name = all_parent_categories[idx]
         parent_id = parents.index(parent_name)
@@ -31,7 +28,7 @@ def add_node_to_structure(results_tmp, bias, all_new_nodes,
         else:
             node_parent['children'] = [node]
         counter += 1
-    return (results_tmp, list_new_nodes)
+    return results_tmp
 
 # Get data to xls file.
 import xlrd
@@ -44,44 +41,46 @@ major_categories = anet_sheet.col_slice(3, 1, 'none')
 # Change data type to get a list of string.
 all_major_categories = [x.value.encode('ascii', 'ignore')
                         for x in major_categories]
+# Removing repeated categories
+majors = list(set(all_major_categories))
 # Create structure in tree to major category.
 results = {}
 node_root = results[0] = {}
 node_root['name'] = 'root'
-majors = []   # List of major categories not repeated
-for index in range(0, len(all_major_categories)):
-    major_node_name = all_major_categories[index]
-    idx_major_node = all_major_categories.index(major_node_name)
-    if idx_major_node < index:
-        continue
-
-    majors.append(major_node_name)
-    major_node = results[index+1] = {}
-    major_node['name'] = major_node_name
+# majors = []   # List of major categories not repeated.
+count = 1
+for index in majors:
+    major_node = results[count] = {}
+    major_node['name'] = index
     if 'children' in node_root:
         node_root['children'].append(major_node)
     else:
         node_root['children'] = [major_node]
+    count += 1
 
 # Find second tier categories in sheet.
 second_tier_categories = anet_sheet.col_slice(2, 1, 'none')
 # Change data type to get a list of string.
 all_second_tier = [x.value.encode('ascii', 'ignore')
                    for x in second_tier_categories]
+# Removing repeated categories
+second_tier = list(set(all_second_tier))
 # Add nodes of second tier category.
 offset = 1   # previous node is only root
-results, second_tier = add_node_to_structure(results, offset, all_second_tier,
-                                             all_major_categories, majors)
+results = add_node_to_structure(results, offset, all_second_tier,
+                                all_major_categories, majors)
 
 # Find third tier category in sheet.
 third_tier_categories = anet_sheet.col_slice(1, 1, 'none')
 # Change data type to get a list of string.
 all_third_tier = [x.value.encode('ascii', 'ignore')
                   for x in third_tier_categories]
+# Removing repeated categories
+third_tier = list(set(all_third_tier))
 # Add nodes of third categories.
 offset += len(majors)
-results, third_tier = add_node_to_structure(results, offset, all_third_tier,
-                                            all_second_tier, second_tier)
+results = add_node_to_structure(results, offset, all_third_tier,
+                                all_second_tier, second_tier)
 
 # Find activity names list in sheet.
 activity_name_categories = anet_sheet.col_slice(0, 1, 'none')
@@ -90,9 +89,8 @@ all_activity_name = [x.value.encode('ascii', 'ignore')
                      for x in activity_name_categories]
 # Add nodes of third categories.
 offset += len(second_tier)
-results, activity_name = add_node_to_structure(results, offset,
-                                               all_activity_name,
-                                               all_third_tier, third_tier)
+results = add_node_to_structure(results, offset, all_activity_name,
+                                all_third_tier, third_tier)
 # Save structure in json file.
 anetData = results[0]
 import json
